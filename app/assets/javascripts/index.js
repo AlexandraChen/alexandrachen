440afe18b144e6d5af0cfc3af13598ecf051c192
Alexandra Chen <alechen04@gmail.com>
2016-01-30 12:15:02 -0500

    Changed navbar color when not on index

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -21,14 +21,5 @@ $(document).ready(function () {
    })
   })
 
-      setTimeout(function() {
-    $(".flash-msg").fadeOut().empty();
-  }, 5000);
-  
-  $(".flash-msg").on("click",function(){
-    $(this).fadeOut();
-   })
-
-
 })
 
